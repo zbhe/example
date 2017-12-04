51e5879b999ee77f3fd348aeff3bf6b5e7f06e3c
bobo he <bobo@bobodeMacBook-Pro.local>
2017-12-04 18:22:50 +0800

    for variadic template

--- template.cpp
+++ template.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include <functional>
-template< typename R, typename ... Args > using TFunc = std::function<R(Args...)>;
-template< typename R > using TCB = std::function<void(R)>;
 
-template <class R, class... Arg> void Invok( std::function<auto(Arg...)> Func, Arg... Paras, TCB<auto> CB)
+template <class CBT, class FUNCT, class... Arg> void Invok(CBT CB, FUNCT Func, Arg... Paras )
 {
+	std::cout << "ONE~\n";
 	CB( Func(Paras...) );
 }
-/*
-template <class R> void Invok( TFunc<R> Func, TCB<R> CB)
+
+template <class R, class... Arg> void Invok( void(CB)(R), R(Func)(Arg...), Arg... Paras )
 {
-	CB( Func() );
+	std::cout << "TWO~\n";
+	CB( Func(Paras...) );
 }
-*/
+
+template <class FUNCT, class... Args> void Call(FUNCT Func, Args... Paras)
+{
+	(void)std::initializer_list<int>{(Func(Paras), 0)...};
+}
 
 int Sum(int x, int y)
 {
@@ -25,14 +29,16 @@ void print( int v )
 
 int main()
 {
-	Invok<int, int, int>(Sum, 3, 5, print);
-	Invok<int, int>([](int x)->int{return x+99;}, 1, print);
-	Invok<std::string>([]()->std::string{
-					return "abc";
-				},
-			[](const std::string& v){
-				std::cout << "string:" << v << std::endl;
-			}
-		);
+	Invok(print, Sum, 3, 5);
+	Invok(print, [](int x)->int{return x+99;}, 1);
+	Invok(
+		[](const std::string& s){
+			std::cout << s << std::endl;
+		},
+		[]()->std::string{
+			return "abc";
+		}
+	);
+	Call(print, 32,343,25,345,65,7,6887,98,5425,214,9889,54,42);
 	return 0;
 }
