20301ff5fb3dd9883bfbfa8649d18cfce3fa4074
bobo he <bobo@bobodeMacBook-Pro.local>
2017-12-12 16:29:31 +0800

    var arg

--- hana.cpp
+++ hana.cpp
@@ -4,12 +4,32 @@
 #include <string>
 #include <iostream>
 namespace hana = boost::hana;
-using namespace std;
-struct Fish{
-	string name;
+//using namespace std;
+struct Fish
+{
+	std::string name;
 };
-struct Mac{
+struct Mac
+{
 	int size;
+};
+#define REFLECTION
+class Person
+{
+	public:
+#ifdef REFLECTION
+		BOOST_HANA_DEFINE_STRUCT(Person,
+			(std::string, name),
+			(int, age),
+			(std::string, address)
+		);
+#else
+		std::string name;
+		int age;
+		std::string address;
+#endif
+	public:
+		Person(const std::string& _name, int _age, const std::string& _address):name(_name), age(_age), address(_address){}
 };
 
 int main()
@@ -18,7 +38,14 @@ int main()
 	auto HasName = hana::is_valid([](auto&& Obj)->decltype(Obj.name){});
 	auto NamedList = hana::remove_if(Tup, [HasName](auto&& Obj){return !HasName(Obj);});
 	hana::for_each(NamedList, [](auto&& Obj){
-		cout << Obj.name << endl;
+			std::cout << Obj.name << std::endl;
 	});
+	Person Bobo{"bobo", 35, "广州..."};
+	std::cout << "size Person:" << sizeof(Person) << std::endl;
+#ifdef REFLECTION
+	hana::for_each(Bobo, [](auto pair){
+		std::cout << hana::to<const char*>(hana::first(pair)) << ":" << hana::second(pair) << std::endl;
+	});
+#endif
 	return 0;
 }
--- vararg.cpp
+++ vararg.cpp
@@ -1,13 +1,20 @@
-#include <stdio.h>
-using namespace std;
-class Print{
-	public:
-		Print(...){
-			printf(__VA_ARGS__);
-		}
-};
+#include <iostream>
 
+using namespace std;
+template<typename T> void Print(T&& t)
+{
+	cout << t << endl;
+}
+
+template<typename H, typename... T> void Print(H&& h, T... t)
+{
+	cout << h << ", ";
+	Print(t...);
+}
+
+#define print(...) Print("__VA_ARGS__ DEMO", ##__VA_ARGS__)
 int main(){
-	Print p("number %d, %d\n", 1, 2);
+	print("number", 1, 2);
+	print();
 	return 0;
 }
