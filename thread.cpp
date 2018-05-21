e1a0fddc283360f587319bfeaf5b5922676d6952
bobo he <bobo@MacBook.local>
2018-05-21 19:59:43 +0800

    thread test

--- thread.cpp
+++ thread.cpp
@@ -1,35 +1,31 @@
-#include<thread>
 #include<vector>
 #include <stdlib.h>
 #include <unistd.h>
 #include <iostream>
-class T{
-	int* p;
-	public:
-		T(){
-			p = new int[100];
-		}
-		~T(){
-			delete [] p;
-			p = NULL;
-			usleep(1000);
-		}
-		void Execute(){
-			for(int i = 0; i < 100; i++ )
-				p[i] = random();
-		}
-};
+#include<thread>
+#include <atomic>
 
-void func(T* pT){
-	while(true){
-		pT->Execute();
+int Count = 0;
+std::atomic<int> ACount{0};
+
+template<typename T> void Inc(T& C)
+{
+	for(int i =0; i < 1000; i++){
+		C++;
 	}
 }
-T Obj5;
 int main(){
-	T Obj4;
-	std::thread t1(func, &Obj4);
-	usleep(10);
-	exit(0);
+	std::vector<std::thread> VThread;
+	VThread.push_back( std::thread{Inc<int>, &Count} );
+	/*VThread.emplace_back( std::thread{Inc<int>, &Count} );
+
+	VThread.emplace_back( std::thread{Inc<decltype(ACount)>, &ACount} );
+	VThread.emplace_back( std::thread{Inc<decltype(ACount)>, &ACount} );
+	*/
+	for( auto& T : VThread){
+		T.join();
+	}
+	std::cout << "Count:" << Count << std::endl;
+	std::cout << "ACount:" << ACount << std::endl;
 	return 0;
 }
